82a9996735ef3ec364f1a7f8e0e683674ffe4b22
Alice Lee <lichoiyin@gmail.com>
2025-02-24 22:11:32 +0800

    Update PastEventsHeader.jsx
    1) Added correct back link
    2) import CSS style
    3) h1 text for "PAST EVENTS"

--- src/components/PastEventsHeader/PastEventsHeader.jsx
+++ src/components/PastEventsHeader/PastEventsHeader.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './PastEventsHeader.css';
 
 const PastEventsHeader = () => {
   return (
     <header className="past-events-header">
-      <div className="header-content">
-
-        <Link to="/" className="back-button">
-          <svg 
-            width="24" 
-            height="24" 
-            viewBox="0 0 24 24" 
-            fill="none" 
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path 
-              d="M19 12H5M12 19l-7-7 7-7" 
-              stroke="currentColor" 
-              strokeWidth="2" 
-              strokeLinecap="round" 
-              strokeLinejoin="round"
-            />
-          </svg>
-          <span>Back</span>
-        </Link>
-
-
+      <div className="back-link">
+        <Link to="/" className="back-button">Back</Link>
+      </div>
+      <div className="title-container">
         <h1 className="header-title">PAST EVENTS</h1>
       </div>
     </header>
