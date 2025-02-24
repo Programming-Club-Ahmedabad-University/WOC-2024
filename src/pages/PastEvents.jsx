97b5f95a30405a82f8cf424bd55df3b373363041
Alice Lee <lichoiyin@gmail.com>
2025-02-24 22:12:58 +0800

    Update PastEvents.jsx
    Import PastEventsHeader jsx file

--- src/pages/PastEvents.jsx
+++ src/pages/PastEvents.jsx
@@ -1,12 +1,13 @@
-import React from 'react'
-import PastEventsHeader from 'src/components/PastEventsHeader';
+import React from 'react';
+import PastEventsHeader from '../components/PastEventsHeader/PastEventsHeader';
 
 const PastEvents = () => {
   return (
     <div>
-      
+      <PastEventsHeader />
+
     </div>
   )
 }
 
-export default PastEvents
+export default PastEvents;
